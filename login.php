0a57426ab9a89a1971680cca3015ea561a8864a2
Wobbabobba <83212933+Wobbabobba@users.noreply.github.com>
2022-02-09 11:23:19 +0100

    login.php, index.html

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+session_start(); 
+$sql = "SELECT * FROM forumlogin";
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databas = "webbserverprogramering";
+
+$conn = new mysqli($servername, $username, $password, $databas);
+$result = $conn->query($sql);
+
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		if($row["UserID"] == $_POST["username"] &&
+					$row["PASSWORD"] == $_POST["password"]) {
+			$login_success = true;
+			
+			}		
+	}
+} 
+
+if($login_success) {
+	$_SESSION["username"] = $_POST["username"];
+    echo "login success";
+}
+
+
+
+
+$conn->close();
+
+?>
+
+
+
+</body>
+</html>
