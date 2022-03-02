630caf4caa6e172f3388382cbab0eaf7b02589f8
Wobbabobba <83212933+Wobbabobba@users.noreply.github.com>
2022-03-02 16:02:38 +0100

    start with threads, index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Forum</title>
+  </head>
+  <body>
+    <h1>Forum</h1>
+    <p>Hejsan och välkommen</p>
+    <button onclick="window.location.href='login.html'">Login</button>
+    <button onclick="window.location.href='register.html'">Register</button>
+  </body>
+</html>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databas = "webbserverprogramering";
+
+$conn = new mysqli($servername, $username, $password, $databas);
+
+$result = $conn->query("SELECT * FROM forumthreads");
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo '<hr><h2> <a href="/">'. $row['thr_name']. ' </a></h2>' .  'From: ' . $row['thr_creator'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . 'Time: ' . $row['thr_date'];
+      }
+}
+
+$conn->close();
+
+
+?>
